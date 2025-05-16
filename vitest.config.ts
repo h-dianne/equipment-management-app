ad09ff1f0ab37a1857d265b35a379733744237ae
Dianne Hidalgo <h-dianne@dainichi-consul.co.jp>
2025-05-16 16:34:27 +0900

    feat: テスト環境をjsdomからhappy-domに変更

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    include: ['src/tests/**/*.test.ts', 'src/tests/**/*.test.tsx'],
-    setupFiles: 'src/tests/setup.ts',
-  },
+    environment: "happy-dom",
+    include: ["src/tests/**/*.test.ts", "src/tests/**/*.test.tsx"],
+    setupFiles: "src/tests/setup.ts"
+  }
 });
